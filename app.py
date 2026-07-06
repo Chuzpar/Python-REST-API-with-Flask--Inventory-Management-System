318cc28daedcf74e32325d64373d7a350745b79a
Yasin <yasinhassan004@gamil.com>
2026-07-06 15:04:56 +0300

    Create the mock database

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+inventory = [
+    {
+        "id": 1,
+        "product_name": "Organic Almond Milk",
+        "brands": "silk",
+        "ingredients_text": "Filtered water, almonds, cane sugar",
+        "price": 4.99,
+        "stock": 12
+    },
+    {
+        "id": 2,
+        "product_name": "Peanut Butter",
+        "brands": "Skippy",
+        "ingredients_text": "Peanuts, sugar, vegetable oils",
+        "price": 3.49,
+        "stock": 20
+    }
+]
+
+@app.route("/")
+def home():
+    return "Inventory API is running!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
